6d374c28f4646878e503481bc3b8fdd1594dcb86
duratm <duratmathias@gmail.com>
2025-06-12 00:13:53 +0200

    updated dependencies

--- operator/internal/controller/stunnergateway_controller.go
+++ operator/internal/controller/stunnergateway_controller.go
@@ -6,14 +6,18 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"time"
 
-	cachev1alpha1 "github.com/Razano26/Nebula/api/v1alpha1"
+	cachev1alpha1 "github.com/Razano26/Nebula/operator/api/v1alpha1"
 )
 
 // StunnerIngressReconciler reconciles a StunnerIngress object
