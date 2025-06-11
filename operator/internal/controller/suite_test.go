b867341bd2d07c10ce614ee8a9ccb276991c673b
duratm <duratmathias@gmail.com>
2025-06-11 23:45:53 +0200

    add flag

--- operator/internal/controller/suite_test.go
+++ operator/internal/controller/suite_test.go
@@ -33,7 +33,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	cachev1alpha1 "github.com/Razano26/Nebula/api/v1alpha1"
+	ingressv1alpha1 "github.com/Razano26/Nebula/operator/api/v1alpha1"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -77,7 +77,7 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
-	err = cachev1alpha1.AddToScheme(scheme.Scheme)
+	err = ingressv1alpha1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	// +kubebuilder:scaffold:scheme
